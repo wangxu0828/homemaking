96b7704762b8b4b2c2e4c5e1cd145da14f5bef74
wangxu0828 <2245267509@qq.com>
2021-10-13 11:13:50 +0800

    分类筛选功能完成

--- pages/home/home.js
+++ pages/home/home.js
@@ -2,10 +2,27 @@ Page({
   data: {
     tabs: ['全部服务', '在提供', '正在找'],
     currentTabNumber: 0,
+    categoryList: [
+      {
+        "id": 1,
+        "name": "保洁"
+      },
+      {
+        "id": 2,
+        "name": "汽修"
+      },
+      {
+        "id": 3,
+        "name": "疏通"
+      }
+    ]
   },
   handlerTabChange(event) {
     this.setData({
       currentTabNumber: event.currentTarget.dataset.index,
     })
   },
+  handleCategoryChange(e) {
+    console.log(e.currentTarget.dataset.id);
+  }
 })
